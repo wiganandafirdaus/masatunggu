650099356b13c2ce1380100a95a56d48024fd8cc
Wigananda Firdaus <157473975+wiganandafirdaus@users.noreply.github.com>
2026-01-04 10:48:38 +0700

    backup project

---
+++ app/Http/Controllers/PrediktorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Prediktor;
+use Illuminate\Http\Request;
+
+class PrediktorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Prediktor $prediktor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Prediktor $prediktor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Prediktor $prediktor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Prediktor $prediktor)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_12_23_120732_create_prediktors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prediktors', function (Blueprint $table) {
+            $table->id();
+            $table->integer('masa_studi');
+            $table->string('provinsi');
+            $table->string('prodi');
+            $table->decimal('ipk',3,2);
+            $table->integer('toefl');
+            $table->boolean('jenis_kelamin')->default(false);//false=cowo
+            $table->integer('sskm');
+            $table->string('nilai_kp');
+            $table->string('nilai_ta');
+            $table->boolean('magang')->default(false);//false=belum magang
+            $table->string('masa_carikerja');
+            $table->integer('jml_lamaran');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prediktors');
+    }
+};
